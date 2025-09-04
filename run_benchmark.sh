02372371acc5a5c6cf900bf486c063dad6470ffe
Simon Johnsson <simzter@outlook.com>
2025-09-04 18:02:33 +0200

    add: updated benchmark utility and new results

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
+# Utility for running a coreutils command against a set of inputs,
+# benchmarking the GNU version in comparison to the uutils version.
 
-if [ "$#" -ne 2 ]; then
-    echo "Wrong number of arguments given. Usage: ./run_benchmark.sh <coreutil cmd with args> <input>"
+if [ "$#" -lt 2 ]; then
+    echo "Wrong number of arguments given. Usage: ./run_benchmark.sh <coreutil cmd with args> [input files]"
 fi
 
-COMMAND="${1}"
-INPUT="${2}"
-UUTIL_COMMAND="coreutils ${COMMAND}"
+command="${1}"
+uutil_command="coreutils ${command}"
 
-FILENAME="$(basename -- "${INPUT}")"
-LOG_FILE="${COMMAND}_comparison_${FILENAME}"
+mkdir "${command}"
+echo "[INFO] Generated directory for output: ${command}"
 
-touch "${LOG_FILE}"
-hyperfine "${COMMAND} ${INPUT}" "${UUTIL_COMMAND} ${INPUT}" -N --warmup 3 >&1 | tee "${LOG_FILE}"
-echo "[Generated with run_benchmark.sh]" >> "${LOG_FILE}"
+for i in "${@:2}"
+do
+    # Create a benchmark for each file
+    input=$i
+    filename="$(basename -- "${input}")"
+    log_file="${command}_comparison_${filename}"
+
+    touch "${log_file}"
+    hyperfine "${command} ${input}" "${uutil_command} ${input}" -N --warmup 3 >&1 | tee "${log_file}"
+    echo "[Generated with run_benchmark.sh]" >> "${log_file}"
+
+    mv "${log_file}" "${command}/"
+    echo "[INFO] Moved log to directory: ${command}"
+    echo ""
+done
